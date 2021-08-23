196f91b0fc52616788178662e2d6e5d1ba737815
alexeyk500 <alexeyk500@yandex.ru>
2021-08-23 18:33:16 +0300

    Model for User done

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,23 @@
-console.log('I am server');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+
+const PORT = config.get('serverPort')
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      config.get('dbUrl'),
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    app.listen(PORT, ()=>{
+      console.log(`Server started on Port=${PORT}`)
+    })
+  } catch (e) {
+      console.log(e.message)
+  }
+}
+
+start()
