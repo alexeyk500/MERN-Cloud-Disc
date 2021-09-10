0a1ee620281b22ea69308fc5448fd2c2dc9f0813
alexeyk500 <alexeyk500@yandex.ru>
2021-09-10 15:31:42 +0300

    15. React + Node.js - Drag and drop загрузка файлов

--- client/src/components/Disk/Disk.tsx
+++ client/src/components/Disk/Disk.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {StateType} from "../../strore/store";
 import {getFiles, uploadFile} from "../../api/fileApi";
@@ -11,6 +11,8 @@ const Disk:React.FC = () => {
 
   const dispatch = useDispatch();
   const currentDir = useSelector((state:StateType) => state.file.currentDir);
+
+  const [dragEnter, setDragEnter] = useState<boolean>(false)
 
   useEffect(()=>{
     dispatch(getFiles(currentDir))
@@ -32,38 +34,75 @@ const Disk:React.FC = () => {
     })
   }
 
+  function onDragEnter(event: React.MouseEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    setDragEnter(true);
+  }
+
+  function onDragLeave(event: React.MouseEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    setDragEnter(false);
+  }
+
+  function onDrop(event: React.DragEvent<HTMLDivElement>) {
+    event.preventDefault();
+    event.stopPropagation();
+    const files = Array.from(event.dataTransfer.files)
+    files.forEach(file=>{
+      dispatch(uploadFile(file, currentDir))
+    })
+    setDragEnter(false);
+  }
+
   return (
-    <div className='disk'>
-      <div className="disk__title">
-        Disk
-      </div>
-      <button
-        className="disk__button_back"
-        onClick={onClickButtonBack}
+    !dragEnter?
+      <div
+        className='disk'
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDragOver={onDragEnter}
       >
-        Back
-      </button>
-      <div className="disk__buttons">
-        <button
-          className="disk__button_create"
-          onClick={onClickButtonCreate}
-        >
-          Create Folder
-        </button>
-        <div className="disk__upload">
-          <label htmlFor="disk__upload_input" className="disk__upload_label">Load File</label>
-          <input
-            type="file"
-            id="disk__upload_input"
-            className="disk__upload_input"
-            multiple={true}
-            onChange={onChangeInputFile}
-          />
+        <div className="disk__title">
+          Disk
         </div>
+        <button
+          className="disk__button_back"
+          onClick={onClickButtonBack}
+        >
+          Back
+        </button>
+        <div className="disk__buttons">
+          <button
+            className="disk__button_create"
+            onClick={onClickButtonCreate}
+          >
+            Create Folder
+          </button>
+          <div className="disk__upload">
+            <label htmlFor="disk__upload_input" className="disk__upload_label">Load File</label>
+            <input
+              type="file"
+              id="disk__upload_input"
+              className="disk__upload_input"
+              multiple={true}
+              onChange={onChangeInputFile}
+            />
+          </div>
+        </div>
+        <FileList />
+        <PopUp />
       </div>
-      <FileList />
-      <PopUp />
-    </div>
+      :<div
+        className="drag_area"
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDragOver={onDragEnter}
+        onDrop={onDrop}
+      >
+        Drag yours files here ...
+      </div>
   );
 };
 
